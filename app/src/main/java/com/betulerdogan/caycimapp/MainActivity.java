6df01b496d8916474d34276d3e13afa133344d59
betulerdogan <betulerdo6an@gmail.com>
2019-06-22 22:04:43 +0300

    drawable eklendi, gradle hatası düzeltildi

--- app/src/main/java/com/betulerdogan/caycimapp/MainActivity.java
+++ app/src/main/java/com/betulerdogan/caycimapp/MainActivity.java
@@ -2,12 +2,34 @@ package com.betulerdogan.caycimapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignUp, btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
